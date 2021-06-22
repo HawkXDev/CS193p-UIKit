8c49b6fd422c066a5dff33051edcd9b043dd913b
Sergey Sokolkin <sergsokolkin@gmail.com>
2021-06-22 14:31:25 +0300

    Add Animations

--- AnimationCards/AnimationCards/PlayingCardVC.swift
+++ AnimationCards/AnimationCards/PlayingCardVC.swift
@@ -10,36 +10,128 @@ import UIKit
 
 class PlayingCardVC: UIViewController {
     
-    var deck = PlayingCardDeck()
+    private var deck = PlayingCardDeck()
     
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left, .right]
-            playingCardView.addGestureRecognizer(swipe)
-            
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
+    @IBOutlet private var cardViews: [PlayingCardView]!
     
-    @objc func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
-        }
-    }
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    
+    lazy var collisionBehavior: UICollisionBehavior = {
+       let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(behavior)
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = false
+        behavior.elasticity = 1.0
+        behavior.resistance = 0
+        animator.addBehavior(behavior)
+        return behavior
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default: break
+        
+        var cards = [PlayingCard]()
+        
+        for _ in 1...((cardViews.count + 1) / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+            
+            collisionBehavior.addItem(cardView)
+            itemBehavior.addItem(cardView)
+            
+            let push = UIPushBehavior(items: [cardView], mode: .instantaneous)
+            push.angle = (2 * CGFloat.pi).random
+            push.magnitude = CGFloat(1.0) + CGFloat(2.0).random
+            push.action = { [unowned push] in
+                push.dynamicAnimator?.removeBehavior(push)
+            }
+            animator.addBehavior(push)
         }
     }
     
+    private var faceUpCardViews: [PlayingCardView] {
+        return cardViews.filter { $0.isFaceUp && !$0.isHidden }
+    }
+    
+    private var faceUpCardViewsMatch: Bool {
+        return faceUpCardViews.count == 2 &&
+            faceUpCardViews[0].rank == faceUpCardViews[1].rank &&
+            faceUpCardViews[0].suit == faceUpCardViews[1].suit
+    }
+    
+    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView {
+                UIView.transition(with: chosenCardView,
+                                  duration: 0.6,
+                                  options: [.transitionFlipFromLeft]) {
+                    chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                } completion: { finished in
+                    if self.faceUpCardViewsMatch {
+                        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.6,
+                                                                       delay: 0,
+                                                                       options: []) {
+                            self.faceUpCardViews.forEach {
+                                $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+                            }
+                        } completion: { position in
+                            UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.75,
+                                                                           delay: 0,
+                                                                           options: []) {
+                                self.faceUpCardViews.forEach {
+                                    $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                    $0.alpha = 0
+                                }
+                            } completion: { position in
+                                self.faceUpCardViews.forEach {
+                                    $0.isHidden = true
+                                    $0.alpha = 1
+                                    $0.transform = .identity
+                                }
+                            }
+                        }
+
+                    } else if self.faceUpCardViews.count == 2 {
+                        self.faceUpCardViews.forEach { cardView in
+                            UIView.transition(with: cardView,
+                                              duration: 0.6,
+                                              options: [.transitionFlipFromLeft]) {
+                                cardView.isFaceUp = false
+                            }
+                        }
+                    }
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+}
+
+extension CGFloat {
+    var random: CGFloat {
+        if self > 0 {
+            return CGFloat.random(in: 0..<self)
+        } else if self < 0 {
+            return -CGFloat.random(in: 0..<abs(self))
+        } else {
+            return 0
+        }
+    }
 }
