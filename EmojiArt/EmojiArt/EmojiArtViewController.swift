d54f03f66d32eb207377a65b15563786d69f8616
Sergey Sokolkin <sergsokolkin@gmail.com>
2021-07-02 15:56:39 +0300

    Correct scaling using configuration constraints in the code

--- EmojiArt/EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -26,6 +26,9 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
     
     var emojiArtView = EmojiArtView()
     
+    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
             scrollView.minimumZoomScale = 0.1
@@ -33,6 +36,11 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
             scrollView.delegate = self
             scrollView.addSubview(emojiArtView)
         }
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        scrollViewHeight.constant = scrollView.contentSize.height
+        scrollViewWidth.constant = scrollView.contentSize.width
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -49,6 +57,10 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
             let size = newValue?.size ?? CGSize.zero
             emojiArtView.frame = CGRect(origin: CGPoint.zero, size: size)
             scrollView.contentSize = size
+            
+            scrollViewHeight?.constant = size.height
+            scrollViewWidth?.constant = size.width
+            
             if let dropZone = self.dropZone, size.width > 0, size.height > 0 {
                 scrollView.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height / size.height)
             }
